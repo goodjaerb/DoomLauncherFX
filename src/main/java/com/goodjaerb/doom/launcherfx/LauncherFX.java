520c46a913d713fa9acc0d764149f097ff6ee481
goodjaerb <goodjaerb@gmail.com>
2017-12-04 09:20:27 -0500

    added sha values for the iwads to various versions of doom/doom2/heretic/hexen with the idea i'll use them to identify games. but i can't do any testing with them right now.

--- src/main/java/com/goodjaerb/doom/launcherfx/LauncherFX.java
+++ src/main/java/com/goodjaerb/doom/launcherfx/LauncherFX.java
@@ -1158,4 +1158,39 @@ public class LauncherFX extends Application {
                     new WarpListItem("E6M1", "6 1"), 
                     new WarpListItem("E6M2", "6 2"), 
                     new WarpListItem("E6M3", "6 3")));
+    
+    private final List<String> DOOM_SHA_VALUES = Collections.unmodifiableList(Arrays.asList(
+            "7742089b4468a736cadb659a7deca3320fe6dcbd", // Doom Version 1.9
+            "2c8212631b37f21ad06d18b5638c733a75e179ff", // Doom Version 1.8
+            "2e89b86859acd9fc1e552f587b710751efcffa8e", // Doom Version 1.666
+            "b5f86a559642a2b3bdfb8a75e91c8da97f057fe6", // Doom Version 1.2
+            "df0040ccb29cc1622e74ceb3b7793a2304cca2c8")); // Doom Version 1.1
+    
+    private final List<String> ULTIMATE_DOOM_SHA_VALUES = Collections.unmodifiableList(Arrays.asList(
+            "9b07b02ab3c275a6a7570c3f73cc20d63a0e3833", // Doom Version 1.9ud
+            "e5ec79505530e151ff0e6f517f3ce1fd65969c46")); // Doom BFG Edition
+    
+    private final List<String> DOOM2_SHA_VALUES = Collections.unmodifiableList(Arrays.asList(
+            "7ec7652fcfce8ddc6e801839291f0e28ef1d5ae7", // Doom 2 Version 1.9
+            "a59548125f59f6aa1a41c22f615557d3dd2e85a9", // Doom 2 BFG Edition
+            "d510c877031bbd5f3d198581a2c8651e09b9861f", // Doom 2 Version 1.8f
+            "79c283b18e61b9a989cfd3e0f19a42ea98fda551", // Doom 2 Version 1.8
+            "70192b8d5aba65c7e633a7c7bcfe7e3e90640c97", // Doom 2 Version 1.7a
+            "78009057420b792eacff482021db6fe13b370dcc", // Doom 2 Version 1.7
+            "6d559b7ceece4f5ad457415049711992370d520a", // Doom 2 Version 1.666
+            "a4ce5128d57cb129fdd1441c12b58245be55c8ce")); // Doom 2 Version 1.666g
+    
+    private final List<String> HERETIC_EXP_SHA_VALUES = Collections.unmodifiableList(Arrays.asList(
+            "f489d479371df32f6d280a0cb23b59a35ba2b833")); // Heretic Version 1.3 (Shadow of the Serpent Riders)
+    
+    private final List<String> HERETIC_SHA_VALUES = Collections.unmodifiableList(Arrays.asList(
+            "A54C5D30629976A649119C5CE8BABAE2DDFB1A60", // Heretic Version 1.2
+            "B5A6CC79CDE48D97905B44282E82C4C966A23A87")); // Heretic Version 1.0
+    
+    private final List<String> HEXEN_SHA_VALUES = Collections.unmodifiableList(Arrays.asList(
+            "4b53832f0733c1e29e5f1de2428e5475e891af29")); // Hexen Version 1.1
+            
+    private final List<String> HEXEN_EXP_SHA_VALUES = Collections.unmodifiableList(Arrays.asList(
+            "081f6a2024643b54ef4a436a85508539b6d20a1e", // Hexen: Deathkings of the Dark Citadel Version 1.1
+            "c3065527d62b05a930fe75fe8181a64fb1982976")); // Hexen: Deathkings of the Dark Citadel Version 1.0
 }
