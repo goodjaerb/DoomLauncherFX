80c5f7445decbedaa673fa4def17662997eed1b3
goodjaerb <goodjaerb@gmail.com>
2019-04-15 23:52:08 -0400

    updated gradle and for jdk12.

---
+++ src/main/java/com/goodjaerb/doom/launcherfx/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.goodjaerb.doom.launcherfx;
+
+/**
+ *
+ * @author goodjaerb
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        // Workaround because JDK11+ javafx applications must be modular but gradle
+        // puts the javafx jars in the classpath, and java doesn't like that.
+        // This makes the Main class not extend javafx, and legally calls the javafx main method.
+        LauncherFX.main(args);
+    }
+}
