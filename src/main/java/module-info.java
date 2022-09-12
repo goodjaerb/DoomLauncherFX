2775e98e11c72a195fedc30b094f0abfde0e665d
goodjaerb <goodjaerb@gmail.com>
2022-09-11 20:36:38 -0400

    suddenly had to add to module-info when it worked fine before?

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.goodjaerb.doom.launcherfx {
     requires java.xml.bind;
     requires javafx.controls;
     requires ini4j;
+    requires org.apache.commons.lang3;
 
     exports com.goodjaerb.doom.launcherfx;
 }
